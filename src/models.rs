881ed4699f395f3d4b874f74530d95c839cd575b
linux_china <libing.chen@gmail.com>
2020-08-25 13:34:05 -0700

    add file_path exists check

--- src/models.rs
+++ src/models.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::Result;
 use std::fs;
 use std::env;
+use std::path::Path;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Settings {
@@ -27,8 +28,13 @@ impl Settings {
     pub fn load() -> Settings {
         let home = env::var("HOME").unwrap();
         let setting_json_path = home + "/.tgm/settings.json";
-        let setting_json = fs::read_to_string(setting_json_path)
-            .expect("Failed to read ~/.tgm/settings.json");
-        serde_json::from_str(setting_json.as_str()).unwrap()
+        let file_path = Path::new(&setting_json_path);
+        if file_path.exists() {
+            let setting_json = fs::read_to_string(setting_json_path)
+                .expect("Failed to read ~/.tgm/settings.json");
+            serde_json::from_str(setting_json.as_str()).unwrap()
+        } else {
+            Settings { templates: vec![], variables: vec![] }
+        }
     }
 }
