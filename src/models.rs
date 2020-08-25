a11e209c8633a10eebcd976d42964aa9df87bd82
linux_china <libing.chen@gmail.com>
2020-08-25 13:29:03 -0700

    models: Settings, Template, Variable

---
+++ src/models.rs
@@ -0,0 +1,34 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::fs;
+use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Settings {
+    pub templates: Vec<Template>,
+    pub variables: Vec<Variable>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Template {
+    pub name: String,
+    pub repository: String,
+    pub description: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Variable {
+    pub name: String,
+    pub value: String,
+    pub description: String,
+}
+
+impl Settings {
+    pub fn load() -> Settings {
+        let home = env::var("HOME").unwrap();
+        let setting_json_path = home + "/.tgm/settings.json";
+        let setting_json = fs::read_to_string(setting_json_path)
+            .expect("Failed to read ~/.tgm/settings.json");
+        serde_json::from_str(setting_json.as_str()).unwrap()
+    }
+}
