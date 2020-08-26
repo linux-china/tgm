2a2b0996d97148b7db9b188af1bb7a68b3e73ecc
linux_china <libing.chen@gmail.com>
2020-08-25 22:05:04 -0700

    add test methods

--- tests/json_tests.rs
+++ tests/json_tests.rs
@@ -1,7 +1,7 @@
-use std::env;
-use std::fs;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
+use std::env;
+use std::fs;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Person {
@@ -17,20 +17,26 @@ pub struct Contact {
     address: String,
 }
 
-
 #[test]
 fn test_path() {
     let home = env::var("HOME").unwrap();
-    let file_name = home + "/.tgm/templates.json";
-    let contents = fs::read_to_string(file_name)
-        .expect("Something went wrong reading the file");
+    let file_name = home + "/.tgm/settings.json";
+    let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file");
     println!("{}", contents);
 }
 
 #[test]
 fn test_json() -> Result<()> {
-    let contact = Contact { email: "libing.chen@gmail.com".into(), phones: vec!["185".into(), "186".into()], address: "中国杭州".into() };
-    let person = Person { name: String::from("linux_china"), age: 40, contact };
+    let contact = Contact {
+        email: "libing.chen@gmail.com".into(),
+        phones: vec!["185".into(), "186".into()],
+        address: "中国杭州".into(),
+    };
+    let person = Person {
+        name: String::from("linux_china"),
+        age: 40,
+        contact,
+    };
     let json_text = serde_json::to_string(&person).unwrap();
     println!("{}", json_text);
     let person2: Person = serde_json::from_str(json_text.as_str())?;
